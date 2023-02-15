73dee008e115662fa7d18f0050e22bb482b14468
Pedro Zanatta <pedrozanattascariot@gmail.com>
2023-02-14 22:56:24 -0300

    feat: invest page

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -3,23 +3,25 @@ import { FooterLinks } from "./FooterLinks";
 
 export const Footer: React.FC = () => {
   return (
-    <Flex
-      w="100vw"
-      h="15.0625rem"
-      borderTopRadius="2xl"
-      bgColor={"#1789A3"}
-      px="4rem"
-      py="3rem"
-      gap="10rem"
-    >
-      <Flex flexDir={"column"}>
-        <Img src={"images/livnlogotext.png"} />
-        <Text fontSize={"sm"} fontWeight="400">
-          Viva investindo
-        </Text>
-      </Flex>
-      <Flex>
-        <FooterLinks />
+    <Flex bgColor="#ffffff">
+      <Flex
+        w="100vw"
+        h="15.0625rem"
+        borderTopRadius="2xl"
+        bgColor={"#1789A3"}
+        px="4rem"
+        py="3rem"
+        gap="10rem"
+      >
+        <Flex flexDir={"column"}>
+          <Img src={"images/livnlogotext.png"} />
+          <Text fontSize={"sm"} fontWeight="400">
+            Viva investindo
+          </Text>
+        </Flex>
+        <Flex>
+          <FooterLinks />
+        </Flex>
       </Flex>
     </Flex>
   );
