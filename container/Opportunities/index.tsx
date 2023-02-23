4d2086a218cc81b0fb955c6e09868b39d5040c6e
Pedro Zanatta <pedrozanattascariot@gmail.com>
2023-02-23 13:12:46 -0300

    fix: opportunities fixes

--- container/Opportunities/index.tsx
+++ container/Opportunities/index.tsx
@@ -7,46 +7,69 @@ import { OpportunitiesCards } from "../../components";
 export const OpportunitiesContainer: FunctionComponent = () => {
   return (
     <DefaultTemplate>
-      <Flex flexDirection="column" bgColor="#ffffff">
+      <Flex
+        flexDirection="column"
+        bgColor="#ffffff"
+        mb="11.0625rem"
+        justifyContent="center"
+      >
         <Flex w="100%">
           <Img w="100%" h="21.3125rem" src="images/opportunitiesBanner.png" />
         </Flex>
         <Flex
           px="1.5rem"
-          justifyContent="space-between"
           mt="2.9375rem"
+          flexDirection="column"
+          justifyContent="center"
           alignItems="center"
         >
-          <Text
+          <Flex
+            alignItems={[
+              "unset",
+              "unset",
+              "unset",
+              "start",
+              "center",
+              "center",
+            ]}
+            flexWrap="wrap"
+            gap="1.5rem"
+            flexDirection={["unset", "unset", "unset", "column", "row", "row"]}
             fontFamily="Poppins"
-            fontStyle="normal"
-            fontWeight="400"
-            fontSize="0.875rem"
-            lineHeight="1.25rem"
-            color="#2D3748"
           >
-            Ordenar por
-          </Text>
-          <MenuInputs />
-          <Text
-            fontFamily="Poppins"
-            fontStyle="normal"
-            fontWeight="400"
-            fontSize="0.875rem"
-            lineHeight="1.25rem"
-            color="#2D3748"
-          >
-            147 resultados
-          </Text>
-        </Flex>
-        <Flex
-          mt="2.9375rem"
-          px="1.5rem"
-          w="100%"
-          justifyContent="center"
-          mb="11.0625rem"
-        >
-          <OpportunitiesCards />
+            <Text fontSize="0.875rem" lineHeight="1.25rem" color="#2D3748">
+              Ordenar por
+            </Text>
+
+            <Flex
+              alignItems={[
+                "unset",
+                "unset",
+                "unset",
+                "start",
+                "center",
+                "center",
+              ]}
+              flexWrap="wrap"
+              gap="1.9375rem"
+              flexDirection={[
+                "unset",
+                "unset",
+                "unset",
+                "column",
+                "row",
+                "row",
+              ]}
+            >
+              <MenuInputs />
+              <Text fontSize="0.875rem" lineHeight="1.25rem" color="#2D3748">
+                147 resultados
+              </Text>
+            </Flex>
+          </Flex>
+          <Flex mt="2.9375rem" w="100%" justifyContent="center">
+            <OpportunitiesCards />
+          </Flex>
         </Flex>
       </Flex>
     </DefaultTemplate>
