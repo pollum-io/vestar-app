62ce9f997fe10b86af30c94016c01b115023d504
Gabriel Prediger <61945626+GabrielPrediger@users.noreply.github.com>
2023-02-24 11:57:08 -0300

    feat: add pie chart design on hover

--- components/Portfolio/PieChart.tsx
+++ components/Portfolio/PieChart.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent, useCallback, useState } from "react";
 import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
 
 const data = [
@@ -27,33 +27,59 @@ const renderCustomizedLabel = ({
       x={x}
       y={y}
       fill="white"
-      textAnchor={x > cx ? "start" : "end"}
+      textAnchor={"middle"}
       dominantBaseline="central"
     >
       {`${(percent * 100).toFixed(0)}%`}
     </text>
   );
 };
-export default class PieChartPortfolio extends PureComponent {
-  render() {
-    return (
-      <PieChart width={280} height={400}>
-        <Pie
-          data={data}
-          cx={120}
-          cy={200}
-          innerRadius={60}
-          outerRadius={120}
-          fill="#8884d8"
-          paddingAngle={0}
-          dataKey="value"
-          label={renderCustomizedLabel}
-        >
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-          ))}
-        </Pie>
-      </PieChart>
-    );
-  }
+
+const renderActiveShape = (props: any) => {
+  const { cx, cy, innerRadius, outerRadius, startAngle, endAngle, fill } =
+    props;
+
+  return (
+    <Sector
+      cx={cx}
+      cy={cy}
+      innerRadius={innerRadius}
+      outerRadius={outerRadius + 5}
+      startAngle={startAngle}
+      endAngle={endAngle}
+      fill={fill}
+    />
+  );
+};
+export default function PieChartPortfolio() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const onPieEnter = useCallback(
+    (_: any, index: any) => {
+      setActiveIndex(index);
+    },
+    [setActiveIndex]
+  );
+  return (
+    <PieChart width={280} height={400}>
+      <Pie
+        activeIndex={activeIndex}
+        activeShape={renderActiveShape}
+        data={data}
+        cx={120}
+        cy={210}
+        innerRadius={60}
+        outerRadius={120}
+        fill="#8884d8"
+        paddingAngle={0}
+        dataKey="value"
+        onMouseEnter={onPieEnter}
+        label={renderCustomizedLabel}
+        labelLine={false}
+      >
+        {data.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+        ))}
+      </Pie>
+    </PieChart>
+  );
 }
