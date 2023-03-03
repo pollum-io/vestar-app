1f524aa11d3f68b847eb572eed949068bca50d09
pmarangone <patrickmarangone@gmail.com>
2023-03-03 08:55:31 -0300

    fix: build error

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,4 @@
 export * from "./Login";
-export * from "../components";
 export * from "./Inputs/DeafultInput/DefaultInput";
 export * from "./Inputs/MenuInput/MenuInput";
 export * from "./Opportunities/OpportunitiesCard/OpportunitiesCard";
