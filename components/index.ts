b8a1ed11bf2fa66bd5783a199b297753827bb6a4
Gabriel Prediger <gabrielprediger046@gmail.com>
2023-03-06 17:50:54 -0300

    chore: build error

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,4 @@
 export * from "./Login";
-export * from "../components";
 export * from "../components/Inputs/DeafultInput/DefaultInput";
 export * from "../components/Invest/PaymentMethods/PaymentMethods";
 export * from "./Inputs/DeafultInput/DefaultInput";
