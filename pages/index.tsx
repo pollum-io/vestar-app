f5d277f0aaef9b11d290a028a48d1d3822cacab1
Gabriel Prediger <61945626+GabrielPrediger@users.noreply.github.com>
2023-03-12 12:14:21 -0300

    feat: add validation at login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,40 @@
+import jwt_decode from "jwt-decode";
 import { LoginContainer } from "../container";
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 
 const Login: NextPage = () => <LoginContainer />;
 
 export default Login;
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+	const token = req.cookies["livn_auth"];
+
+	if (!token) {
+		return {
+			redirect: {
+				permanent: false,
+				destination: "/",
+			},
+			props: {},
+		};
+	}
+
+	const user: any = jwt_decode(token);
+
+	if (!user?.investor_id) {
+		return {
+			redirect: {
+				permanent: false,
+				destination: "/register",
+			},
+			props: {},
+		};
+	}
+
+	return {
+		props: {
+			user,
+			token,
+		},
+	};
+};
