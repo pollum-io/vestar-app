15cfb68154701dc620e7878b20b617ec06d6438a
Gabriel Prediger <61945626+GabrielPrediger@users.noreply.github.com>
2023-03-12 12:14:21 -0300

    feat: add validation at login page

--- contexts/user.tsx
+++ contexts/user.tsx
@@ -1,21 +1,17 @@
 import React, { createContext, useState, useMemo } from "react";
 
-interface IRegister {
-
-}
+interface IRegister {}
 
 export const UserContext = createContext({} as IRegister);
 
 export const UserProvider: React.FC<{ children: React.ReactNode }> = ({
 	children,
 }) => {
+	const [isUserLogged, setIsUserLogged] = useState<boolean>(false);
 
 	const providerValue = useMemo(
-		() => ({
-
-		}),
-		[
-		]
+		() => ({ isUserLogged, setIsUserLogged }),
+		[isUserLogged]
 	);
 
 	return (
