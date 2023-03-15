6e2bfbd6816617b9883869b770fb18fe0879d04a
Gabriel Prediger <gabrielprediger046@gmail.com>
2023-03-15 11:52:43 -0300

    fix: changed px to rem

--- container/Forgot_Password/index.tsx
+++ container/Forgot_Password/index.tsx
@@ -269,11 +269,11 @@ export const Forgot_PasswordContainer = () => {
 						_hover={
 							validCode || !isCode
 								? {
-										cursor: "pointer",
-										bgColor: "#007D99",
-										boxShadow:
-											"0px 1px 3px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.06)",
-								  }
+									cursor: "pointer",
+									bgColor: "#007D99",
+									boxShadow:
+										"0px 1px 3px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.06)",
+								}
 								: { opacity: "0.3" }
 						}
 						onClick={
@@ -296,7 +296,7 @@ export const Forgot_PasswordContainer = () => {
 							<Text
 								fontStyle="normal"
 								fontWeight="normal"
-								fontSize="12px"
+								fontSize="0.75rem"
 								lineHeight="150%"
 								color="#2D3748"
 								display={!isCode ? "none" : "flex"}
