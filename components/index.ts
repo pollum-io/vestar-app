c8cd76b6875a9524d33d46be33f61211761e48c0
Gabriel Prediger <gabrielprediger046@gmail.com>
2023-03-15 11:55:47 -0300

    chore: build error

--- components/index.ts
+++ components/index.ts
@@ -4,7 +4,7 @@ export * from "../components/Invest/PaymentMethods/PaymentMethods";
 export * from "./Inputs/DeafultInput/DefaultInput";
 export * from "./Inputs/MenuInput/MenuInput";
 export * from "./Opportunities/OpportunitiesCard/OpportunitiesCard";
-export * from "./Toasty/ToastyCard/ToastyCard";
+export * from "./Toasty/ToastyCard/toastyCard";
 export * from "./Companies/CompaniesCard/CompaniesCard";
 export * from "./Companie/CompanieDetails/CompanieDetails";
 export * from "./Companie/CompanieContact/CompanieContact";
