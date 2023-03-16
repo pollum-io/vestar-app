0af756a6773d4fbea60f7013ddc6810189d29da9
Gabriel Prediger <61945626+GabrielPrediger@users.noreply.github.com>
2023-03-16 19:34:07 -0300

    fix: routes validation

--- pages/investir.tsx
+++ pages/investir.tsx
@@ -1,6 +1,40 @@
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 import { InvestContainer } from "../container";
+import jwt_decode from "jwt-decode";
 
 const Investir: NextPage = () => <InvestContainer />;
 
 export default Investir;
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+	const token = req.cookies["livn_auth"];
+
+	if (!token) {
+		return {
+			redirect: {
+				permanent: false,
+				destination: "/",
+			},
+			props: {},
+		};
+	}
+
+	const user: any = jwt_decode(token);
+
+	if (!user?.investor_id) {
+		return {
+			redirect: {
+				permanent: false,
+				destination: "/registrar",
+			},
+			props: {},
+		};
+	}
+
+	return {
+		props: {
+			user,
+			token,
+		},
+	};
+};
