fe66d4e17fa7dda7c991d5292875f358359d4a77
Gabriel Prediger <gabrielprediger046@gmail.com>
2023-03-21 00:31:45 -0300

    feat: investor and enterprise validation on PriceCard component

--- pages/oportunidades/[id].tsx
+++ pages/oportunidades/[id].tsx
@@ -2,21 +2,37 @@ import { GetServerSideProps, NextPage } from "next";
 import { ImovelContainer } from "../../container/Imovel/index";
 import { IOpportunitiesCard } from "../../dtos/Oportunities";
 import { fetchImovelDetail } from "../../services/imovelDetail";
+import jwt_decode from "jwt-decode";
 
 interface IImovelProps {
 	data: IOpportunitiesCard;
+	users: any;
 }
 
-const Imovel: NextPage<IImovelProps> = ({ data }) => {
-	return <ImovelContainer imovel={data} />;
+const Imovel: NextPage<IImovelProps> = ({ data, users }) => {
+	return <ImovelContainer imovel={data} usersId={users} />;
 }
-export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+export const getServerSideProps: GetServerSideProps = async ({ req, query }) => {
+	const token = req.cookies["livn_auth"];
+
+	if (!token) {
+		return {
+			redirect: {
+				permanent: false,
+				destination: "/",
+			},
+			props: {},
+		};
+	}
+
+	const user: any = jwt_decode(token);
 
 	const response = await fetchImovelDetail(query.id)
 
 	return {
 		props: {
-			data: response.data
+			data: response.data,
+			users: user
 		}
 	}
 }
