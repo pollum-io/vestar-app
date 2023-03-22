4eaa21a555991348413762ef93ac7298b728cc55
Gabriel Prediger <61945626+GabrielPrediger@users.noreply.github.com>
2023-03-21 21:57:25 -0300

    feat: Mask

--- container/Edit_Profile/index.tsx
+++ container/Edit_Profile/index.tsx
@@ -13,12 +13,19 @@ import { formatPhoneNumber } from "../../utils/formatPhoneNumber";
 import { SelectComponent } from "../../components/Select/SelectComponent";
 
 const estadosCivis = [
-	{ id: 1, name: 'Solteiro(a)' },
-	{ id: 2, name: 'Casado(a)' },
-	{ id: 3, name: 'União Estável' },
-	{ id: 4, name: 'Viúvo(a)' },
-	{ id: 5, name: 'Separado(a) judicialmente' },
-	{ id: 6, name: 'Divorciado(a)' }
+	{ id: 1, name: "Solteiro(a)" },
+	{ id: 2, name: "Casado(a)" },
+	{ id: 3, name: "União Estável" },
+	{ id: 4, name: "Viúvo(a)" },
+	{ id: 5, name: "Separado(a) judicialmente" },
+	{ id: 6, name: "Divorciado(a)" },
+];
+
+const estadosRegimesPatrimoniais = [
+	{ id: 1, name: "Comunhão parcial de bens" },
+	{ id: 2, name: "Comunhão universal de bens)" },
+	{ id: 3, name: "Separação total de bens" },
+	{ id: 4, name: "Participação final nos aquestos)" },
 ];
 interface IEditProfile {
 	data: any;
@@ -28,7 +35,8 @@ export const Edit_ProfileContainer: FunctionComponent<any> = props => {
 	const { data, token } = props;
 	const [pagePath, setPagePath] = useState("personal");
 	const [isDisabled, setIsDisabled] = useState(true);
-	const [isMarried, setIsMarried] = useState<any>("");
+	const [maritalStatus, setMaritalStatus] = useState<any>("");
+	const [equityRegime, setEquityRegime] = useState<any>("");
 	const {
 		register,
 		handleSubmit,
@@ -36,49 +44,58 @@ export const Edit_ProfileContainer: FunctionComponent<any> = props => {
 		formState: { isSubmitSuccessful },
 		reset,
 	} = useForm();
-	const { userInfos } = useUser()
+	const { userInfos } = useUser();
 	const { toast } = useToasty();
-	const dataFormatada = new Date(data?.birthday_date).toISOString().split('T')[0]
+	const dataFormatada = new Date(data?.birthday_date)
+		.toISOString()
+		.split("T")[0];
+	console.log(maritalStatus, "maritalStatus");
 
-	console.log(data, "Dados vindo da API")
+	console.log(data, "Dados vindo da API");
 
 	const onSubmitForm = async (data: any) => {
-		console.log(data, 'Dados chegando na funcao')
+		console.log(data, "Dados chegando na funcao");
 
-		const request =
-		{
+		const request = {
 			full_name: data.full_name,
 			mother_name: data.mother_name,
 			city_of_birth: data.city_of_birth,
 			birthday_date: new Date(data.birthday_date),
-			cpf: data.cpf.replace(/[^\w]/gi, '').replace(/\s+/g, ''),
+			cpf: data.cpf.replace(/[^\w]/gi, "").replace(/\s+/g, ""),
 			rg: data.rg,
 			cnh: data.cnh,
-			marital_status: isMarried,
+			marital_status: {
+				status: maritalStatus,
+				equity_regime: equityRegime,
+				spouse_name: data.spouse_name,
+				spouse_cpf: data.spouse_cpf,
+				spouse_rg: data.spouse_rg,
+			},
 			address: data.address,
 			profession: data.profession,
 			email: data.email,
-			phone_number: data.phone_number.replace(/[^\w]/gi, '').replace(/\s+/g, ''),
-		}
+			phone_number: data.phone_number
+				.replace(/[^\w]/gi, "")
+				.replace(/\s+/g, ""),
+		};
 
-		await fetchEditInvestor(userInfos, request, token).then(res => {
-			if (res) {
-				console.log(res, 'res')
+		await fetchEditInvestor(userInfos, request, token)
+			.then(res => {
+				if (res) {
+					console.log(res, "res");
 
-				toast({
-					id: "toast-edit",
-					position: "top-right",
-					status: "success",
-					title: "Dados editados!",
-					description:
-						"Os seus dados foram atualizados!",
-				});
-			}
-		})
+					toast({
+						id: "toast-edit",
+						position: "top-right",
+						status: "success",
+						title: "Dados editados!",
+						description: "Os seus dados foram atualizados!",
+					});
+				}
+			})
 			.catch(err => {
 				console.log({ err });
 			});
-
 	};
 
 	return (
@@ -152,15 +169,50 @@ export const Edit_ProfileContainer: FunctionComponent<any> = props => {
 										maxWidth="18.5rem"
 										gap="1.5rem"
 									>
-										<Flex flexDirection="column" gap="0.25rem" mb="2.75rem"
-										>
-											<InputComponent label="Nome" type="text" {...register("full_name")} defaultValue={data?.full_name} />
-											<InputComponent label="Nome da Mãe" type="text" {...register("mother_name")} defaultValue={data?.mother_name} />
-											<InputComponent label="Cidade de nascimento" type="text" {...register("city_of_birth")} defaultValue={data?.city_of_birth} />
-											<InputComponent label="Data de Nascimento " type="date" {...register("birthday_date")} defaultValue={dataFormatada} />
-											<InputComponent label="CPF" type="text" maxLength={14} {...register("cpf")} defaultValue={formatCPF(data?.cpf)} />
-											<InputComponent label="RG" type="text" {...register("rg")} defaultValue={data?.rg} />
-											<InputComponent label="CNH" type="text" {...register("cnh")} defaultValue={data?.cnh} />
+										<Flex flexDirection="column" gap="0.25rem" mb="2.75rem">
+											<InputComponent
+												label="Nome"
+												type="text"
+												{...register("full_name")}
+												defaultValue={data?.full_name}
+											/>
+											<InputComponent
+												label="Nome da Mãe"
+												type="text"
+												{...register("mother_name")}
+												defaultValue={data?.mother_name}
+											/>
+											<InputComponent
+												label="Cidade de nascimento"
+												type="text"
+												{...register("city_of_birth")}
+												defaultValue={data?.city_of_birth}
+											/>
+											<InputComponent
+												label="Data de Nascimento "
+												type="date"
+												{...register("birthday_date")}
+												defaultValue={dataFormatada}
+											/>
+											<InputComponent
+												label="CPF"
+												type="text"
+												maxLength={14}
+												{...register("cpf")}
+												defaultValue={formatCPF(data?.cpf)}
+											/>
+											<InputComponent
+												label="RG"
+												type="text"
+												{...register("rg")}
+												defaultValue={data?.rg}
+											/>
+											<InputComponent
+												label="CNH"
+												type="text"
+												{...register("cnh")}
+												defaultValue={data?.cnh}
+											/>
 										</Flex>
 									</Flex>
 									<Flex
@@ -172,17 +224,74 @@ export const Edit_ProfileContainer: FunctionComponent<any> = props => {
 										lineHeight="1.25rem"
 										w="20rem"
 									>
-										<SelectComponent defaultValue={data.marital_status} setIsMarried={setIsMarried} label="Estado Civil" type="marital" selectValue={estadosCivis} {...register("marital_status")} />
-										<Collapse in={isMarried === "Casado(a)"}>
-											<InputComponent label="Regime Patrimonial" type="text" {...register("address")} defaultValue={data?.address} />
-											<InputComponent label="Nome Completo do Cônjuge" type="text" {...register("address")} defaultValue={data?.address} />
-											<InputComponent label="CPF do Cônjuge" type="text" {...register("address")} defaultValue={formatCPF(data?.cpf)} />
-											<InputComponent label="RG do Cônjuge" type="text" {...register("address")} defaultValue={data?.address} />
+										<SelectComponent
+											defaultValue={data?.marital_status?.status}
+											setData={setMaritalStatus}
+											label="Estado Civil"
+											type="marital"
+											selectValue={estadosCivis}
+											{...register("status")}
+										/>
+										<Collapse
+											in={
+												maritalStatus === "Casado(a)" ||
+												data?.marital_status?.status === "Casado(a)"
+											}
+										>
+											<SelectComponent
+												defaultValue={data.marital_status?.equity_regime}
+												setData={setEquityRegime}
+												label="Regime Patrimonial"
+												type="regime_patrimonial"
+												selectValue={estadosRegimesPatrimoniais}
+												{...register("equity_regime")}
+											/>
+											<InputComponent
+												label="Nome Completo do Cônjuge"
+												type="text"
+												{...register("spouse_name")}
+												defaultValue={data?.marital_status?.spouse_name}
+											/>
+											<InputComponent
+												label="CPF do Cônjuge"
+												type="text"
+												{...register("spouse_cpf")}
+												defaultValue={formatCPF(
+													data?.marital_status?.spouse_cpf
+												)}
+											/>
+											<InputComponent
+												label="RG do Cônjuge"
+												type="text"
+												{...register("spouse_rg")}
+												defaultValue={data?.marital_status?.spouse_rg}
+											/>
 										</Collapse>
-										<InputComponent label="Endereço Residencial" type="text" {...register("address")} defaultValue={data?.address} />
-										<InputComponent label="Ocupação Profissional" type="text" {...register("profession")} defaultValue={data?.profession} />
-										<InputComponent label="Email" type="email" {...register("email")} defaultValue={data?.email} />
-										<InputComponent label="Telefone" type="text" maxLength={15} {...register("phone_number")} defaultValue={formatPhoneNumber(data?.phone_number)} />
+										<InputComponent
+											label="Endereço Residencial"
+											type="text"
+											{...register("address")}
+											defaultValue={data?.address}
+										/>
+										<InputComponent
+											label="Ocupação Profissional"
+											type="text"
+											{...register("profession")}
+											defaultValue={data?.profession}
+										/>
+										<InputComponent
+											label="Email"
+											type="email"
+											{...register("email")}
+											defaultValue={data?.email}
+										/>
+										<InputComponent
+											label="Telefone"
+											type="text"
+											maxLength={15}
+											{...register("phone_number")}
+											defaultValue={formatPhoneNumber(data?.phone_number)}
+										/>
 									</Flex>
 								</Flex>
 								<Flex w="100%" justifyContent="flex-start">
