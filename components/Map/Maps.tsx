f1e7fcfd625301e16012f377edb78768674e8e0d
Gabriel Prediger <61945626+GabrielPrediger@users.noreply.github.com>
2023-03-23 21:49:16 -0300

    feat: maps multi localization

--- components/Map/Maps.tsx
+++ components/Map/Maps.tsx
@@ -12,6 +12,8 @@ export const Maps: FunctionComponent<IMaps> = ({
 	localizations,
 }) => {
 	const [getLocalization, setGetLocalization] = useState<any>([]);
+	const [getLocalizations, setGetLocalizations] = useState<any>([]);
+
 	const [data, setData] = useState<any>([]);
 	const { isLoaded } = useLoadScript({
 		googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_APY_KEY as any,
@@ -20,7 +22,6 @@ export const Maps: FunctionComponent<IMaps> = ({
 		if (localization) {
 			fetchGeocode(localization as any).then(res => {
 				setData(res);
-				console.log(res, "res");
 			});
 		} else if (localizations) {
 			localizations?.map((data: any) => {
@@ -30,17 +31,29 @@ export const Maps: FunctionComponent<IMaps> = ({
 			});
 		}
 	}, [localization, localizations]);
-	console.log(data?.results, "data?.results?");
+	//aqui nessa funcao eu mando o address e recebo um array com todos os dados do lugar
 
 	useEffect(() => {
+		if (localizations) {
+			const asd = data?.map((data: any) => data?.results).flat();
+			const getPlace = asd?.map((data: any) => data?.geometry?.location);
+
+			setGetLocalizations(getPlace);
+		}
 		const getPlace = data?.results?.map(
 			(data: any) => data?.geometry?.location
 		);
 		setGetLocalization(getPlace);
-	}, [data]);
-	console.log(getLocalization, "getLocalization");
+	}, [data, localizations]);
+	//aqui eu pego o location que tem lat e lng e que sao um array de obj [{}]
+
 	const convertPlace = useMemo(() => getLocalization?.[0], [getLocalization]);
-	console.log(convertPlace, "convertPlace");
+	const convertPlaces = useMemo(
+		() => getLocalizations?.[0],
+		[getLocalizations]
+	);
+
+	//aqui eu so pego o objeto da posicao 0
 
 	return (
 		<>
@@ -48,21 +61,24 @@ export const Maps: FunctionComponent<IMaps> = ({
 				localizations?.length ? (
 					<GoogleMap
 						zoom={16}
-						center={convertPlace}
+						center={convertPlace?.length ? convertPlace : convertPlaces}
 						mapContainerClassName="map-container"
 					>
-						<MarkerF
-							icon={{
-								url: "/images/icons/Home-Maps.svg",
-								scaledSize: new google.maps.Size(57, 57),
-								labelOrigin: new google.maps.Point(30, -32),
-							}}
-							label={{
-								text: "Crypto Plaza",
-								className: "map-label",
-							}}
-							position={convertPlace}
-						/>
+						{getLocalizations.map((places: any) => (
+							// eslint-disable-next-line react/jsx-key
+							<MarkerF
+								icon={{
+									url: "/images/icons/Home-Maps.svg",
+									scaledSize: new google.maps.Size(57, 57),
+									labelOrigin: new google.maps.Point(30, -32),
+								}}
+								label={{
+									text: "Crypto Plaza",
+									className: "map-label",
+								}}
+								position={places}
+							/>
+						))}
 					</GoogleMap>
 				) : (
 					<GoogleMap
