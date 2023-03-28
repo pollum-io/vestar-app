ff229fe11a7ae27c05b7d281e59ac67a0935b2a0
vieiraricardo <hi@ricvieira.com>
2023-03-28 12:04:01 -0300

    fix merge

--- components/Inputs/MenuInput/MenuInput.tsx
+++ components/Inputs/MenuInput/MenuInput.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useCallback } from "react";
 import {
 	Flex,
 	Button,
@@ -9,11 +9,25 @@ import {
 } from "@chakra-ui/react";
 import { IMenuInput } from "./dto";
 import { IoIosArrowDown } from "react-icons/io";
+import { useRouter } from "next/router";
 
 export const MenuInput: FunctionComponent<IMenuInput> = ({
 	placeholder,
 	color,
+	fields,
+	param,
 }) => {
+	const router = useRouter();
+
+	const setParams = useCallback(
+		(param: string, value: any) => {
+			router.query[param] = value;
+			router.push(router);
+		},
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		[router.query]
+	);
+
 	return (
 		<Menu>
 			<MenuButton
@@ -39,12 +53,16 @@ export const MenuInput: FunctionComponent<IMenuInput> = ({
 				{placeholder}
 			</MenuButton>
 			<MenuList bgColor="#ffffff" border="0.0625rem solid #E2E8F0">
-				<MenuItem bgColor="#ffffff" color="#2D3748">
-					Pred
-				</MenuItem>
-				<MenuItem bgColor="#ffffff" color="#2D3748">
-					Pedro
-				</MenuItem>
+				{fields?.map(field => (
+					<MenuItem
+						key={field}
+						bgColor="#ffffff"
+						color="#2D3748"
+						onClick={() => setParams(param, field)}
+					>
+						{field}
+					</MenuItem>
+				))}
 			</MenuList>
 		</Menu>
 	);
@@ -53,10 +71,25 @@ export const MenuInput: FunctionComponent<IMenuInput> = ({
 export const MenuInputs: FunctionComponent = () => {
 	return (
 		<Flex gap="1.5rem">
-			<MenuInput placeholder="Tipo de Imóvel" color="#2D3748" />
-			<MenuInput placeholder="Previsão de Conclusão" color="#2D3748" />
-			<MenuInput placeholder="Investimento Mínimo" color="#2D3748" />
-			<MenuInput placeholder="Localização" color="#A0AEC0" />
+			<MenuInput
+				placeholder="Tipo de Imóvel"
+				color="#2D3748"
+				fields={["Comercial", "Residencial", "Loteamento"]}
+				param="enterprise_type"
+			/>
+			<MenuInput
+				placeholder="Previsão de Conclusão"
+				color="#2D3748"
+				fields={["crescente", "decrescente"]}
+				param="expected_delivery_date"
+			/>
+			<MenuInput
+				placeholder="Investimento Mínimo"
+				color="#2D3748"
+				fields={["min", "max"]}
+				param="min_investment"
+			/>
+			{/* <MenuInput placeholder="Localização" color="#A0AEC0"/> */}
 		</Flex>
 	);
 };
