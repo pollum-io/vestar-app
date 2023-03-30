efdbd21dfa70b6ba9f77657bb45928ef71e1f01c
Gabriel Prediger <gabrielprediger046@gmail.com>
2023-03-30 12:24:22 -0300

    feat: register toast cnpj and enterprise name validation

--- services/fetchLogin.ts
+++ services/fetchLogin.ts
@@ -1,18 +1,15 @@
 export const authenticate = async (email: string, password: string) => {
-	const response = await fetch(
-		"http://localhost:3000/api/user/authenticate",
-		{
-			method: "post",
-			headers: {
-				"content-type": "application/json",
-				accept: "application/json",
-			},
-			body: JSON.stringify({
-				email: email,
-				password: password,
-			}),
-		}
-	).then(res => res.json());
+	const response = await fetch("http://localhost:3000/api/user/authenticate", {
+		method: "post",
+		headers: {
+			"content-type": "application/json",
+			accept: "application/json",
+		},
+		body: JSON.stringify({
+			email: email,
+			password: password,
+		}),
+	}).then(res => res.json());
 
 	return response;
 };
