863af2c3f2ff629001c8c4d7ec606a16da011cdd
Gabriel Prediger <gabrielprediger046@gmail.com>
2023-06-13 10:27:58 -0300

    fix: build error

--- pages/api/investorPJ/index.ts
+++ pages/api/investorPJ/index.ts
@@ -6,7 +6,7 @@ import dbConnect from "../../../lib/dbConnect";
 import User from "../../../models/user";
 import { generateToken, setCookie, verifyUser } from "../../../lib/auth";
 import { ApiResponse } from "../../../models/ApiResponse";
-import investorPJ from "../../../models/investorPJ";
+import investorPJ from "../../../models/investorPj";
 
 interface NextConnectApiRequest extends NextApiRequest {
 	user?: {
