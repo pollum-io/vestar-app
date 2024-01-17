5c9aa0b3c9b3c1a4538bfd3925a4ba5d9404ded8
Gabriel Prediger <gabrielprediger046@gmail.com>
2024-01-17 19:10:07 -0300

    chore: testing things to find build error

--- components/Imovel/Carousel.tsx
+++ components/Imovel/Carousel.tsx
@@ -67,7 +67,7 @@ export const Carousel: React.FC<ICarousel> = props => {
 			}
 
 			Promise.all(
-				orderedImages.map((picture?: string) => api.get(`/api/file/${picture}`))
+				orderedImages.map((picture?: string) => api.get(`/file/${picture}`))
 			).then(responses => {
 				const imageUrls = responses.map(
 					response => response.request?.responseURL
@@ -78,7 +78,7 @@ export const Carousel: React.FC<ICarousel> = props => {
 			const extraImagePromises: Promise<any>[] | undefined = extra_images?.map(
 				(picture: string) =>
 					api
-						.get(`/api/file/${picture}`)
+						.get(`/file/${picture}`)
 						.then(response => response.request?.responseURL)
 			);
 
