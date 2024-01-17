4088150b244deb8ae364e86ff34693b58563118a
Gabriel Prediger <gabrielprediger046@gmail.com>
2024-01-17 19:49:33 -0300

    chore: testing things to find build error

--- services/api.ts
+++ services/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 
 export const apiInstance = (host?: string): AxiosInstance =>
 	axios.create({
-		baseURL: host ? `${host}/api` : "/api",
+		baseURL: host ? `https://${host}/api` : "/api",
 		headers: {
 			"content-type": "application/json",
 			accept: "application/json",
