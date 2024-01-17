103a1cb4bfee00351cf62b0f2417eaf90b30aa2d
Gabriel Prediger <gabrielprediger046@gmail.com>
2024-01-17 19:27:00 -0300

    chore: testing things to find build error

--- services/fetchImovelDetail.ts
+++ services/fetchImovelDetail.ts
@@ -3,10 +3,7 @@ import { apiInstance } from "./api";
 export async function fetchImovelDetail(id: any, host?: string) {
 	try {
 		const api = apiInstance(host);
-		console.log(id, host, "aaaaaaaaaaaa");
 		const response = await api.get(`/opportunity/${id}`);
-		console.log(response, "responseresponse");
-
 		return response.data;
 	} catch (error) {
 		console.error(error);
