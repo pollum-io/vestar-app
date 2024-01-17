70601391c035e584ca91fa09307c8f0132843777
Gabriel Prediger <gabrielprediger046@gmail.com>
2024-01-17 20:20:19 -0300

    chore: changed http url

--- services/api.ts
+++ services/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 
 export const apiInstance = (host?: string): AxiosInstance =>
 	axios.create({
-		baseURL: host ? `https://${host}/api` : "/api",
+		baseURL: host ? `http://${host}/api` : "/api",
 		headers: {
 			"content-type": "application/json",
 			accept: "application/json",
