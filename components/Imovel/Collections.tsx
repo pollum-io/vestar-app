f4cdd52bbfecdb243c0edafc2e6a67ba4cb2554a
Gabriel Prediger <61945626+GabrielPrediger@users.noreply.github.com>
2024-01-18 21:36:06 -0300

    fix: carrousel error

--- components/Imovel/Collections.tsx
+++ components/Imovel/Collections.tsx
@@ -9,7 +9,6 @@ export const Collections: React.FC<ICollections> = props => {
 	const { images } = props;
 	const { isOpen, onOpen, onClose } = useDisclosure();
 	const [selectedImage, setSelectedImage] = useState<any>();
-
 	return (
 		<Flex
 			w="100%"
@@ -21,7 +20,7 @@ export const Collections: React.FC<ICollections> = props => {
 			justifyContent="center"
 		>
 			<CollectionsModal
-				images={images}
+				allImages={images}
 				isOpen={isOpen}
 				onClose={onClose}
 				selectedImage={selectedImage}
@@ -30,13 +29,13 @@ export const Collections: React.FC<ICollections> = props => {
 			<Flex
 				onClick={() => {
 					onOpen();
-					setSelectedImage(images[0]);
+					setSelectedImage(images[0] ?? null);
 				}}
 			>
 				<Img
 					width="34.75rem"
 					height="25rem"
-					src={`/api/file/${images[0]}`}
+					src={`/api/file/${images[0] ?? null}`}
 					borderLeftRadius="0.75rem"
 					_hover={{ cursor: "pointer", filter: "brightness(90%)" }}
 					transition="200ms"
@@ -46,22 +45,22 @@ export const Collections: React.FC<ICollections> = props => {
 				<Img
 					w="17.125rem"
 					h="12.25rem"
-					src={`/api/file/${images[1]}`}
+					src={`/api/file/${images[1] ?? null}`}
 					_hover={{ cursor: "pointer", filter: "brightness(90%)" }}
 					transition="200ms"
 					onClick={() => {
 						onOpen();
-						setSelectedImage(images[1]);
+						setSelectedImage(images[1] ?? null);
 					}}
 				/>
 				<Img
 					w="17.125rem"
 					h="12.25rem"
-					src={`/api/file/${images[2]}`}
+					src={`/api/file/${images[2] ?? null}`}
 					borderTopRightRadius="0.75rem"
 					onClick={() => {
 						onOpen();
-						setSelectedImage(images[2]);
+						setSelectedImage(images[2] ?? null);
 					}}
 					_hover={{ cursor: "pointer", filter: "brightness(90%)" }}
 					transition="200ms"
@@ -69,10 +68,10 @@ export const Collections: React.FC<ICollections> = props => {
 				<Img
 					w="17.125rem"
 					h="12.25rem"
-					src={`/api/file/${images[3]}`}
+					src={`/api/file/${images[3] ?? null}`}
 					onClick={() => {
 						onOpen();
-						setSelectedImage(images[3]);
+						setSelectedImage(images[3] ?? null);
 					}}
 					_hover={{ cursor: "pointer", filter: "brightness(90%)" }}
 					transition="200ms"
@@ -80,11 +79,11 @@ export const Collections: React.FC<ICollections> = props => {
 				<Img
 					w="17.125rem"
 					h="12.25rem"
-					src={`/api/file/${images[4]}`}
+					src={`/api/file/${images[4] ?? null}`}
 					borderBottomRightRadius="0.75rem"
 					onClick={() => {
 						onOpen();
-						setSelectedImage(images[4]);
+						setSelectedImage(images[4] ?? null);
 					}}
 					_hover={{ cursor: "pointer", filter: "brightness(90%)" }}
 					transition="200ms"
