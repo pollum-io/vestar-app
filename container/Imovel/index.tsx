f7c41cf335c3e29c545733154507d7c738765c60
Gabriel Prediger <61945626+GabrielPrediger@users.noreply.github.com>
2024-01-19 22:09:40 -0300

    chore(imovel): added translations

--- container/Imovel/index.tsx
+++ container/Imovel/index.tsx
@@ -381,13 +381,13 @@ export const ImovelContainer: FunctionComponent<IImovelProps> = ({
 			>
 				<Flex flexDir={"column"} w="70rem">
 					<Text mb="2rem" fontWeight={"600"} fontSize="2xl" color={"#f1f1f1"}>
-						Em breve você poderá acompanhar:
+						{t("opportunitieDetails.soon")}{" "}
 					</Text>
 					<Flex gap="2.1875rem">
 						<Flex alignItems={"center"} gap="0.9rem">
 							<Img src={"/icons/home.svg"} />
 							<Text fontWeight={"400"} color={"#f1f1f1"} w="8.5rem">
-								Todas as plantas da obra
+								{t("opportunitieDetails.floorPlans")}{" "}
 							</Text>
 						</Flex>
 						<Flex alignItems={"center"} gap="0.9rem">
@@ -405,14 +405,14 @@ export const ImovelContainer: FunctionComponent<IImovelProps> = ({
 						<Flex alignItems={"center"} gap="0.9rem">
 							<Img src={"/icons/folder.svg"} />
 							<Text fontWeight={"400"} color={"#f1f1f1"} w="75%">
-								Documentos Extras
+								{t("opportunitieDetails.extraDoc")}
 							</Text>
 						</Flex>
 					</Flex>
-					<Flex mt="2rem">
-						<Text color={"#f1f1f1"}>
+					<Flex>
+						{/* <Text color={"#f1f1f1"}>
 							Atualmente esta obra está em estágio de
-						</Text>
+						</Text> */}
 					</Flex>
 				</Flex>
 			</Flex>
@@ -425,7 +425,7 @@ export const ImovelContainer: FunctionComponent<IImovelProps> = ({
 			>
 				<Flex mb={"2rem"} w="100%" maxWidth="70rem">
 					<Text fontSize={"1.5rem"} fontWeight={"600"} color={"#171923"}>
-						Localização
+						{t("opportunities.location")}
 					</Text>
 				</Flex>
 				<Flex maxWidth="70rem">
